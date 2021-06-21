cf66e1404f59603864458296ee4d018f685f6062
ismael712 <vladimirgarcia712@gmail.com>
2021-06-20 18:57:17 -0600

    agregando cambios a la base de datos

--- BibliotecaPNC.sql
+++ BibliotecaPNC.sql
@@ -3,13 +3,13 @@ use Biblioteca;
 drop database Biblioteca;
 
 create table Rol(
-IdRol int identity(0,1) primary key,
+IdRol int identity(1,1) primary key,
 Nombre varchar(30),
 Estado bit default 1
 );
 
 create table Usuario(
-IdUsuario int identity(0,1) primary key,
+IdUsuario int identity(1,1) primary key,
 Username varchar(30) unique,
 Nombres varchar(60),
 Apellidos varchar(60),
@@ -18,21 +18,21 @@ IdRol int foreign key references Rol(idRol)
 );
 
 create table Libro(
-IdLibro int identity(0,1) primary key,
+IdLibro int identity(1,1) primary key,
 Nombre varchar(150),
 Autor varchar(60),
 ISBN varchar(30) --Considero el quitarlo
 );
 
 create table Ejemplar(
-IdEjemplar int identity(0,1) primary key,
+IdEjemplar int identity(1,1) primary key,
 IdLibro int foreign key references Libro(IdLibro),
 Ubicacion varchar(100),
 Estado varchar(30) not null CHECK (Estado in('Disponible', 'Prestado', 'En Reparacion')) default 'Disponible',
 );
 
 create table Prestamo(
-IdPrestamo int identity(0,1) primary key,
+IdPrestamo int identity(1,1) primary key,
 IdEjemplar int not null,
 IdUsuario int not null,
 FechaPrestamo date not null,
@@ -63,6 +63,54 @@ INSERT [dbo].[Libro] ([Nombre], [Autor], [ISBN]) VALUES (N'Cuentos policiales pa
 INSERT [dbo].[Libro] ([Nombre], [Autor], [ISBN]) VALUES (N'Cuentos populares de pícaros y necios', N'Anonimo', N'978-987-24143')
 INSERT [dbo].[Libro] ([Nombre], [Autor], [ISBN]) VALUES (N'Antiguas leyendas de héroes y princesas', N'Jacob Walsh', N'978-987-24143')
 INSERT [dbo].[Libro] ([Nombre], [Autor], [ISBN]) VALUES (N'La isla misteriosa', N'Hanna Lamb', N'978-987-24500')
+
+--Insert Ejemplar
+
+INSERT [dbo].[Ejemplar]([IdLibro], [Ubicacion]) VALUES (1,'Planta Baja')
+INSERT [dbo].[Ejemplar]([IdLibro], [Ubicacion]) VALUES (2,'Primera Planta')
+INSERT [dbo].[Ejemplar]([IdLibro], [Ubicacion]) VALUES (3,'Primera Planta')
+INSERT [dbo].[Ejemplar]([IdLibro], [Ubicacion]) VALUES (4,'Tercer Planta')
+INSERT [dbo].[Ejemplar]([IdLibro], [Ubicacion]) VALUES (5,'Segunda Planta')
+INSERT [dbo].[Ejemplar]([IdLibro], [Ubicacion]) VALUES (6,'Segunda Planta')
+INSERT [dbo].[Ejemplar]([IdLibro], [Ubicacion]) VALUES (7,'Tercer Planta')
+INSERT [dbo].[Ejemplar]([IdLibro], [Ubicacion]) VALUES (8,'Primera Planta')
+INSERT [dbo].[Ejemplar]([IdLibro], [Ubicacion]) VALUES (9,'Planta Baja')
+INSERT [dbo].[Ejemplar]([IdLibro], [Ubicacion]) VALUES (10,'Segunda Planta')
+INSERT [dbo].[Ejemplar]([IdLibro], [Ubicacion]) VALUES (11,'Tercer Planta')
+INSERT [dbo].[Ejemplar]([IdLibro], [Ubicacion]) VALUES (12,'Tercer Planta')
+INSERT [dbo].[Ejemplar]([IdLibro], [Ubicacion]) VALUES (13,'Segunda Planta')
+INSERT [dbo].[Ejemplar]([IdLibro], [Ubicacion]) VALUES (14,'Primera Planta')
+INSERT [dbo].[Ejemplar]([IdLibro], [Ubicacion]) VALUES (15,'Primera Planta')
+INSERT [dbo].[Ejemplar]([IdLibro], [Ubicacion]) VALUES (16,'Segunda Planta')
+INSERT [dbo].[Ejemplar]([IdLibro], [Ubicacion]) VALUES (17,'Planta Baja')
+INSERT [dbo].[Ejemplar]([IdLibro], [Ubicacion]) VALUES (18,'Segunda Planta')
+INSERT [dbo].[Ejemplar]([IdLibro], [Ubicacion]) VALUES (19,'Tercer Planta')
+INSERT [dbo].[Ejemplar]([IdLibro], [Ubicacion]) VALUES (20,'Tercer Planta')
+
+--Insert Rol
+INSERT [dbo].[Rol]([Nombre]) VALUES ('Admin')
+INSERT [dbo].[Rol]([Nombre]) VALUES ('Profesor')
+
+--Insert Usuario
+INSERT [dbo].[Usuario]([Username], [Nombres], [Apellidos], [Password], [IdRol]) VALUES ('aspleitez','Adriana Sofia', 'Pleitez Perez','root', 1)
+INSERT [dbo].[Usuario]([Username], [Nombres], [Apellidos], [Password], [IdRol]) VALUES ('cloarca','Carlos', 'Loarca','rootroot', 1)
+INSERT [dbo].[Usuario]([Username], [Nombres], [Apellidos], [Password], [IdRol]) VALUES ('nexxtor','Nestor', 'Aldana','rootx3', 2)
+
+--Insert Prestamo
+
+INSERT [dbo].[Prestamo]([IdEjemplar], [IdUsuario], [FechaPrestamo], [FechaDevolucion]) VALUES (2,3,'2021-06-06 15:00:00.000','2021-06-15 15:00:00.000')
+INSERT [dbo].[Prestamo]([IdEjemplar], [IdUsuario], [FechaPrestamo]) VALUES (19,3,'2021-06-08 15:00:00.000')
+INSERT [dbo].[Prestamo]([IdEjemplar], [IdUsuario], [FechaPrestamo], [FechaDevolucion]) VALUES (7,2,'2021-06-06 15:00:00.000','2021-06-17 15:00:00.000')
+INSERT [dbo].[Prestamo]([IdEjemplar], [IdUsuario], [FechaPrestamo]) VALUES (10,1,'2021-06-07 15:00:00.000')
+INSERT [dbo].[Prestamo]([IdEjemplar], [IdUsuario], [FechaPrestamo], [FechaDevolucion]) VALUES (14,2,'2021-06-09 15:00:00.000','2021-06-24 15:00:00.000')
+
+
+
+SELECT * FROM LIBRO
+SELECT * FROM Ejemplar
+SELECT * FROM Rol
+SELECT * FROM Usuario
+SELECT * FROM Prestamo
 
 go;
 
@@ -180,4 +228,6 @@ as begin
 		order by FechaPrestamo desc;
 end;
 go;
+
+
 
